cdf9a14eb48484f752216119a88f71b698706dfc
Masiu <ciszekmarcell@gmail.com>
2019-12-10 20:50:04 +0100

    insert route

--- routes/user.js
+++ routes/user.js
@@ -11,6 +11,15 @@ router.get('/', (req, res) => {
     res.json({ data: count });
   });
 });
+
+router.post('/', async (req, res) => {
+  const q = 'INSERT INTO users (email) values(?)';
+  db.query(q, [req.body.email], (err, r) => {
+    if (err) throw err;
+    console.log(r);
+    res.json(r);
+  });
+});
 // db.end();
 
 module.exports = router;
