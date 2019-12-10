4aea974135109a03b776a993ed4e6749622f5de5
Masiu <ciszekmarcell@gmail.com>
2019-12-10 18:15:26 +0100

    setting up api routes

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mysql = require('mysql');
-const faker = require('faker');
+// const faker = require('faker');
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -16,17 +16,17 @@ db.connect(err => {
   console.log('Connected to MySQL DB');
 });
 
-const data = [];
-for (let i = 0; i < 500; i += 1) {
-  data.push([faker.internet.email(), faker.date.past()]);
-}
+// const data = [];
+// for (let i = 0; i < 500; i += 1) {
+//   data.push([faker.internet.email(), faker.date.past()]);
+// }
 
-const q = 'INSERT INTO users (email, created_at) VALUES ?';
+// const q = 'INSERT INTO users (email, created_at) VALUES ?';
 
-db.query(q, [data], function(err, result) {
-  console.log(err);
-  console.log(result);
-});
+// db.query(q, [data], function(err, result) {
+//   console.log(err);
+//   console.log(result);
+// });
 
 // global.db = db;
 
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
 const express = require('express');
-
+const cors = require('cors');
 const db = require('./db');
 
 const app = express();
 const port = 4000 || process.env.PORT;
 
+app.use(cors());
+
 app.use(express.json());
 
+app.use('/api/users', require('./routes/user'));
+
 db.end();
+
 app.listen(port, () => console.log(`port is on ${port}`));
