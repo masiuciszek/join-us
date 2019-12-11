81baa021cc36ef3da4f2530b9a18a4496171cdbc
Masiu <ciszekmarcell@gmail.com>
2019-12-11 20:40:20 +0100

    body parser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const db = require('./db');
 
 const app = express();
@@ -9,7 +10,13 @@ const port = 5000 || process.env.PORT;
 app.use(cors());
 
 // app.use(express.json());
-app.use(express.json({ extended: false }));
+// app.use(express.json({ extended: false }));
+// parse application/x-www-form-urlencoded
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.use('/api/users', require('./routes/user'));
 
